ec1dd4b68d70d5ede87a1f10ab4ad007859cae00
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-18 21:47:09 +0300

    .

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-products',
@@ -7,11 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
-  product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
-  product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
-  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 }
-  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 }
-  product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 }
 
-  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+
+  products:Product[] = [];
 }
