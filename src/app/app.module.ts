27fa32c91d5badcab0999819cf3760552fff0df4
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-18 12:36:57 +0300

    front-end started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+
+  product1 = { productId: 1, productName: 'Bardak', categoryId: 1, unitPrice: 5 }
+  product2 = { productId: 2, productName: 'Laptop', categoryId: 1, unitPrice: 5 }
+  product3 = { productId: 3, productName: 'Mouse', categoryId: 1, unitPrice: 5 }
+  product4 = { productId: 4, productName: 'Keyboard', categoryId: 1, unitPrice: 5 }
+  product5 = { productId: 5, productName: 'Camera', categoryId: 1, unitPrice: 5 }
+
+  products = [this.product1, this.product2, this.product3, this.product4, this.product5];
+}
