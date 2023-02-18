0f04b4b957993a5d82a6550ffcde3164cf5368ba
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-19 01:08:19 +0300

    database connection made

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
 
 
+  constructor(private productService:ProductService) {} // injection yapıldı.
 
-  products:Product[] = [];
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts() .subscribe((response) => {  //url den gelen sonucu ProductResponseModel e map le ve dönecek sonuca abone ol gelen yanıt response 
+      this.products= response.data   //
+      this.dataLoaded = true;
+    }) 
+
+  }
 }
