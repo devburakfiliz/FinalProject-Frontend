9a320df0490af2d761f57d90feb96e32c85c94cc
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-20 21:40:47 +0300

    .

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  dataLoaded = false;
 
+
+  constructor(private categoryService:CategoryService) {} // injection yapıldı.
+
+  ngOnInit(): void {
+   this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getCategories() .subscribe((response) => {  //url den gelen sonucu ProductResponseModel e map le ve dönecek sonuca abone ol gelen yanıt response 
+      this.categories= response.data   //
+      this.dataLoaded = true;
+    }) 
+
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; //backend de ki dataya ulaşmamıozı saglar
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ProductResponseModel> {  
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl)
+  getProducts():Observable<ListResponseModel<Product>> {  
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl)
      
   }
 }
