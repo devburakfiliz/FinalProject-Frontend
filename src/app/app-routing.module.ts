1245a4d52404b99c2787f40521e212991cea63de
Burak Filiz <dev.burakfiliz@gmail.com>
2023-03-05 14:31:28 +0300

    added login service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductsComponent } from './components/products/products.component';
 const routes: Routes = [
@@ -7,6 +8,7 @@ const routes: Routes = [
   {path:"products",component:ProductsComponent},
   {path:"products/category/:categoryId",component:ProductsComponent},
   {path:"products/add",component:ProductAddComponent},
+  {path:"login",component:LoginComponent}
 
 ];
 
